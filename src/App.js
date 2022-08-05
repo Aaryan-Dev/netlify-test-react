60574fb3f5e67951035cd1c515648b279d36307b
Aryan Dev <nobodygivesafak@gmail.com>
2022-08-06 02:40:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import "./App.css";
 // import data from db.json
 
 function App() {
- 
-
   return (
-    <div className="App" data-testid = 'app'>
-      <button  data-testid = 'sort-asc-btn'>Sort by Asc</button>
-      <button  data-testid = 'sort-desc-btn'>Sort by Desc</button>
+    <div className="App" data-testid="app">
+      <button data-testid="sort-asc-btn">Sort by Asc</button>
+      <button data-testid="sort-desc-btn">Sort by Desc</button>
+      <h1>Fuckers</h1>
       {/*  map through the users data and pass props to the Userdetails component */}
     </div>
   );
